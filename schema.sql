481ce9265b7bc687905453294353a81dae5b2ecf
Juuso <juuso.saavalainen@helsinki.fi>
2022-09-25 16:21:46 +0300

    Add temporary  SQL schema

---
+++ schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    password TEXT
+);
+
+CREATE TABLE topic (
+    id SERIAL PRIMARY KEY,
+    creator_id INTEGER REFERENCES users,
+    title TEXT,
+    visible INTEGER
+);
+
+CREATE TABLE comments (
+    id SERIAL PRIMARY KEY,
+    creator_id INTEGER REFERENCES users,
+    topic_id INTEGER REFERENCES topic,
+    comment TEXT
+);
